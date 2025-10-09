b11aa582212e80a32eaeec7d8bc06d1350883b31
p5nbTgip0r <cb3f02c5@opayq.com>
2025-10-09 03:10:51 -0700

    feat: add optional unit parameter to convert command

--- src/commands/convert.rs
+++ src/commands/convert.rs
@@ -1,7 +1,16 @@
 use crate::conversions::glucose::ParsedGlucoseResult;
 use crate::serenity::CreateEmbed;
-use crate::{Context, Error};
 use crate::util::colors::{ERROR, INFO, WARNING};
+use crate::{Context, Error};
+use poise::ChoiceParameter;
+
+#[derive(Debug, poise::ChoiceParameter)]
+pub enum GlucoseUnit {
+    #[name = "mg/dL"]
+    Mgdl,
+    #[name = "mmol/L"]
+    Mmol,
+}
 
 /// Converts blood glucose units (mg/dL <> mmol/L).
 #[poise::command(
@@ -11,8 +20,9 @@ use crate::util::colors::{ERROR, INFO, WARNING};
 pub async fn convert(
     ctx: Context<'_>,
     #[description = "The value to convert (e.g. 5.7mmol, 100 mg, 40)"] glucose: String,
+    #[description = "Blood glucose unit (mmol/L, mg/dL)"] unit: Option<GlucoseUnit>,
 ) -> Result<(), Error> {
-    let reply = match glucose.parse::<ParsedGlucoseResult>() {
+    let reply = match ParsedGlucoseResult::parse(&glucose, unit.map(|u| u.name())) {
         Ok(glucose_value) => match glucose_value {
             ParsedGlucoseResult::Known(bg) => {
                 let embed = CreateEmbed::default().color(INFO).description(format!(
